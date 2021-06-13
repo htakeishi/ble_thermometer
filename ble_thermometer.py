fa5beaf8ba0d1f84ad38a9751ad5f054baec468e
Henri Takeishi <f.daijyu@gmail.com>
2021-06-13 16:01:51 +0900

    refactored the main code.

--- ble_thermometer.py
+++ ble_thermometer.py
@@ -3,15 +3,15 @@ from bleak import BleakClient
 # https://bleak.readthedocs.io/en/latest/api.html#module-bleak.backends.service
 
 address = "B8:80:4F:58:B9:6C" # you can find a thermometer's back panel if UT-201BLE
-UUID =    "00001809-0000-1000-8000-00805f9b34fb" # refer to GATT specification
+THERMOMETER_UUID =    "00001809-0000-1000-8000-00805f9b34fb" # refer to GATT specification
 
 async def run(address):
     client = BleakClient(address)
     try:
         await client.connect()
         if (client.is_connected):
-            ret = await client.read_gatt_char(UUID)
-            print("ret: {0}".format("".join(map(chr, ret))))
+            ret = await client.read_gatt_char(THERMOMETER_UUID)
+            print("tempture: {0}".format("".join(map(chr, ret))))
         else:
             raise Exception
     except Exception as e:
@@ -19,5 +19,9 @@ async def run(address):
     finally:
         await client.disconnect()
 
-loop = asyncio.get_event_loop()
-loop.run_until_complete(run(address))
+def main():
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(run(address))
+
+if __name__ == "__main__":
+    main()
